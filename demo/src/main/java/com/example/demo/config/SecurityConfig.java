1ccc5b7c43da4af0505252798f0b0fe3916b74e3
kaneezfatima5 <kaneezfatimarajper55@gmail.com>
2026-01-24 12:52:23 -0700

    allowing CORS for frontend-1

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -30,8 +31,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> {})
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**", "/api/v1/test", "/api/v1/events", "/api/v1/events/{id}", "/api/v1/files/event-images/**",
                                 "/api/v1/calendar/**").permitAll()
